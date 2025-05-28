b911cdc7908deaf65b0b92ee5a4fa78378f67f18
Hieu Nguyen <shandyprofile@gmail.com>
2025-05-28 17:52:24 +0700

    update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1748429069',resources: [
+    cacheName: 'chirpy-1748429507',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
